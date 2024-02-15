0ef1268a8e0f67954d2bd34fe55a9b7e092387a9
kalyanivyavahare <155135046+kalyanivyavahare@users.noreply.github.com>
2024-02-15 13:07:49 +0530

    changes done

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ async function fetchWeatherCity(cityName){
 	let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=271d9d9a3c7a465c7a4236ca282fc028`;
 const response =await fetch(url)
 const data=await response.json();
-console.log(data)
+let city =document.getElementById("city");
+city.innerHTML=data.name;
+let windSpeed=document.getElementById("wind_speed");
+windSpeed.innerHTML=data.wind.speed+"km/hr";
+let humidity=document.getElementById("humidity");
+humidity.innerHTML=data.main.humidity+"g/kg";
+let temp =document.getElementById("temp");
+temp.innerHTML=Math.round((data.main.temp)-273.15)+"°C";
+Console.log(data);
 }
 function fetchCity(){
 	let cityName=document.getElementById("city_name");
